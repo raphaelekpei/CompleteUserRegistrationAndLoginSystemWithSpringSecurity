542f44d673b60805a1deaa152b1c971a4076c8b3
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 03:26:24 +0100

    Commit 110019

--- src/main/java/com/raphael/completeuserregistrationandloginsystemwithspringsecurity/services/TokenService.java
+++ src/main/java/com/raphael/completeuserregistrationandloginsystemwithspringsecurity/services/TokenService.java
@@ -3,10 +3,6 @@ package com.raphael.completeuserregistrationandloginsystemwithspringsecurity.ser
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
