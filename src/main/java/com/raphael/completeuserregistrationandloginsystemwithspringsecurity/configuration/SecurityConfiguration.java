dad2aa8e0d48ed494059285026f8b30f82c40e5d
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 03:26:40 +0100

    Commit 110020

--- src/main/java/com/raphael/completeuserregistrationandloginsystemwithspringsecurity/configuration/SecurityConfiguration.java
+++ src/main/java/com/raphael/completeuserregistrationandloginsystemwithspringsecurity/configuration/SecurityConfiguration.java
@@ -1,12 +1,6 @@
 package com.raphael.completeuserregistrationandloginsystemwithspringsecurity.configuration;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
-import com.unkownkoder.utils.RSAKeyProperties;
+import com.raphael.completeuserregistrationandloginsystemwithspringsecurity.utils.RSAKeyProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,12 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
