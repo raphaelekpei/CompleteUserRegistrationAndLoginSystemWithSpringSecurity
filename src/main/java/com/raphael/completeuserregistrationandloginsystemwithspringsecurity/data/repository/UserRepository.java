8f24089a43d2783fd2c7b3fce6ffa708e9297302
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 03:26:07 +0100

    Commit 110018

--- src/main/java/com/raphael/completeuserregistrationandloginsystemwithspringsecurity/data/repository/UserRepository.java
+++ src/main/java/com/raphael/completeuserregistrationandloginsystemwithspringsecurity/data/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.raphael.completeuserregistrationandloginsystemwithspringsecurity.data.repository;
 
-import com.unkownkoder.data.models.ApplicationUser;
+import com.raphael.completeuserregistrationandloginsystemwithspringsecurity.data.models.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, Integer> {
